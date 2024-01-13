40f60a2ab06cf2b97abce217171c025e51a32d85
ali emad <aliemadaboshosha13@gmail.com>
2024-01-14 00:20:34 +0200

    change some css in login form and create some JavaScript logic  in Cart

--- javascript/test.js
+++ javascript/test.js
@@ -1,21 +1,54 @@
 // function ActiveButton(e){e.classList.toggle("btn-dark");}
+totalprice();
 function removeItem(itemId) {
     var elementToRemove = document.querySelector('.' + itemId);
     if (elementToRemove) {
         elementToRemove.remove();
     }
+    totalprice()
+
 }
 
 function incrementValue() {
-    var inputElement = document.querySelector('input[type="number"]');
+   var itemPrice=135; //we will get from jesons
+    var price = event.target.parentNode.parentNode.parentNode.getElementsByClassName("Price")[0];
+
+    var inputElement = event.target.parentNode.querySelector('input[type="number"]');
     if (inputElement) {
         inputElement.stepUp();
     }
+price.innerHTML = (itemPrice * Number(inputElement.value))+".00 $";
+
+totalprice()
 }
 
 function decrementValue() {
-    var inputElement = document.querySelector('input[type="number"]');
+    var itemPrice=135; //we will get from jesons
+
+    var price = event.target.parentNode.parentNode.parentNode.getElementsByClassName("Price")[0];
+    var inputElement = event.target.parentNode.querySelector('input[type="number"]');
     if (inputElement) {
         inputElement.stepDown();
     }
+    price.innerHTML = (itemPrice * Number(inputElement.value))+".00 $";
+    totalprice()
+}
+
+function totalprice() {
+var cartItems=document.getElementsByClassName("CartItems")[0].children;
+console.log(cartItems);
+var totalPrice=0
+var itemsNumber=0
+for (let index = 0; index < cartItems.length; index++) {
+    totalPrice+=parseInt(cartItems[index].getElementsByClassName("Price")[0].innerHTML);
+    itemsNumber+=parseInt(cartItems[index].querySelector('input').value);
+    
+}
+
+console.log(totalPrice);
+console.log(itemsNumber);
+console.log(document.querySelector(".CartMenu").children[0].children[0].innerHTML)
+document.querySelector(".total").innerHTML=document.querySelector(".CartMenu").children[0].children[0].innerHTML=totalPrice+".00 $";
+   
+
 }
