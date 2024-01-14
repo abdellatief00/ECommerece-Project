c4993a389b834ac5ec66762ae00ddaf5bd482830
abdellatief <abdellatiefhamed00@gmail.com>
2024-01-14 23:15:21 +0200

    added js file for checkout

---
+++ javascript/checkout.js
@@ -0,0 +1,9 @@
+$(".copon").click(function(){
+    if($(".coponinput").css("display")=="none"){
+        $(".coponinput").css("display","block");
+    }
+    else{
+        $(".coponinput").css("display","none");
+    }
+    
+});
