e41bfd19e35c3c890e7b64ef498e1eb9f2a2d43f
abdellatief <abdellatiefhamed00@gmail.com>
2024-01-15 23:23:47 +0200

    finished my part

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -7,3 +7,22 @@ $(".copon").click(function(){
     }
     
 });
+
+/* your details*/
+window.addEventListener('load',function(){
+    if(this.window.innerWidth<=768){
+        this.document.querySelector('#content .formcontent .rightcontent #collapseOne').classList.remove("show");
+        this.document.querySelector('#content .formcontent .rightcontent .accordion button').classList.add("collapsed");
+        this.document.querySelector('#content .formcontent .rightcontent .accordion button').setAttribute("aria-expanded",'False');
+        }
+})
+window.addEventListener('resize',function(){
+    if(this.window.innerWidth<=768){
+    this.document.querySelector('#content .formcontent .rightcontent #collapseOne').classList.remove("show");
+    }
+    else{
+        this.document.querySelector('#content .formcontent .rightcontent #collapseOne').classList.add("show");
+    }
+
+})
+/* end of your details */
