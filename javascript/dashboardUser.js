50f7211bb85a22f684c81815dd1994ee573da62c
AyaSHEHAT <ayashehata274@yahoo.com>
2024-01-28 21:39:21 +0200

    last last last update

--- javascript/dashboardUser.js
+++ javascript/dashboardUser.js
@@ -5,7 +5,7 @@ import {
 } from './modula.js';
 
 window.addEventListener('load', function () {
-
+    let row_id;
     let userTable_tbody = document.getElementById('tbodyUsers');
     var users = JSON.parse(localStorage.getItem("users"));
     var products = JSON.parse(localStorage.getItem("products"))
@@ -41,7 +41,7 @@ window.addEventListener('load', function () {
                 switch (key) {
                     case "images":
                         let imgTag = document.createElement("img");
-                        imgTag.setAttribute("src", `${key}/${users[i][key]}`)
+                        imgTag.setAttribute("src", `${users[i][key]}`)
                         createdtd.appendChild(imgTag);
                         break;
                     case "role":
@@ -70,61 +70,18 @@ window.addEventListener('load', function () {
             }
             //<i class="fa-regular fa-eye"></i>
             let lastcreatedtd = document.createElement('td');
-            // let showIcon = document.createElement('i');
-            // showIcon.setAttribute("class", "fa-regular fa-eye text-primary cursorPointer");
-            // let showBtn = document.createElement('button');
-            // showBtn.setAttribute("class", "btn m-1");
-            // showBtn.setAttribute("data-bs-toggle", "modal");
-            // showBtn.setAttribute("data-bs-target", "#editViewUser");
-            // showBtn.appendChild(showIcon);
-            // lastcreatedtd.appendChild(showBtn);
-            // showBtn.setAttribute('data-row-index', i);
-            // showBtn.addEventListener('click', function () {
-            //     // Retrieve the stored index
-            //     let index = showBtn.getAttribute('data-row-index');
-            //     document.getElementById("fname").value = users[index]["fname"];
-            //     document.getElementById("lname").value = users[index]["lname"];
-            //     document.getElementById("email").value = users[index]["email"];
-            //     document.getElementById("pass").value = users[index]["password"];
-            //     document.getElementById("age").value = users[index]["age"];
-            //     document.getElementById("fname").setAttribute("disabled", "disabled");
-            //     document.getElementById("lname").setAttribute("disabled", "disabled");
-            //     document.getElementById("email").setAttribute("disabled", "disabled");
-            //     document.getElementById("pass").setAttribute("disabled", "disabled");
-            //     document.getElementById("pass").removeAttribute("type");
-            //     document.getElementById("pass").setAttribute("type", "text");
-            //     document.getElementById("age").setAttribute("disabled", "disabled");
-            //     document.getElementById("editUser").style.display = "none";
-            //     document.getElementById("editViewUserCancel").innerText = "close";
-            //     document.getElementById("editViewUserCancel").classList.remove("btn-danger");
-            //     document.getElementById("editViewUserCancel").classList.add("btn-secondary");
-
-            // });
-
-
             let editIcon = document.createElement('i');
-            //console.log(editIcon);
             editIcon.setAttribute("class", "fa-regular fa-pen-to-square text-info cursorPointer");
-
             lastcreatedtd.appendChild(editIcon);
             let editBtn = document.createElement('button');
             editBtn.setAttribute("class", "btn m-1");
             editBtn.setAttribute("data-bs-toggle", "modal");
-            editBtn.setAttribute("data-bs-target", "#editViewUser");
+            editBtn.setAttribute("data-bs-target", "#editUserModal");
             editBtn.appendChild(editIcon);
 
             editBtn.setAttribute('data-row-index', i);
             editBtn.addEventListener('click', function () {
                 let index = editBtn.getAttribute('data-row-index');
-                // document.getElementById("fname").removeAttribute("disabled");
-                // document.getElementById("lname").removeAttribute("disabled");
-                // document.getElementById("email").removeAttribute("disabled");
-                // document.getElementById("pass").removeAttribute("disabled");
-                // document.getElementById("age").removeAttribute("disabled");
-                // document.getElementById("editUser").style.display = "block";
-                // document.getElementById("editViewUserCancel").innerText = "Cancel";
-                // document.getElementById("editViewUserCancel").classList.add("btn-danger");
-                // document.getElementById("editViewUserCancel").classList.remove("btn-secondary");
                 document.getElementById("fname").value = users[index]["fname"];
                 document.getElementById("lname").value = users[index]["lname"];
                 document.getElementById("email").value = users[index]["email"];
@@ -133,45 +90,33 @@ window.addEventListener('load', function () {
                 document.getElementById("pass").setAttribute("type", "text");
                 document.getElementById("age").value = users[index]["age"];
                 document.getElementById("role").value = users[index]["role"] == 0 ? "user" : "seller";
-                //let imagepath = document.getElementById("image").value = users[index]["images"];
-                document.getElementById("editUser").addEventListener("click", function () {
-                    users[index]["fname"] = document.getElementById("fname").value;
-                    users[index]["lname"] = document.getElementById("lname").value;
-                    users[index]["email"] = document.getElementById("email").value;
-                    users[index]["password"] = document.getElementById("pass").value;
-                    users[index]["age"] = document.getElementById("age").value;
-                    users[index]["role"] = document.getElementById("role").value == "user" ? 0 : 1;
-                    //users[index]["images"]=imagepath.value;
-                    localStorage.setItem("users", JSON.stringify(users));
-                    deleteTable();
-                    // Remove the row from the table
-                    DrawTable(users, rowsNum.value);
-
-                });
+                // document.getElementById("image").value = users[index].images[0];            
             }); // edit button
 
             lastcreatedtd.appendChild(editBtn);
             ///////edit icon
-
-
             let deleteIcon = document.createElement('i');
             deleteIcon.setAttribute("class", "fa-regular fa-trash-can text-danger cursorPointer");
             let deleteBtn = document.createElement('button');
-            deleteBtn.setAttribute("class", "btn m-1");
+            deleteBtn.setAttribute("class", "btn m-1 border border-0");
             deleteBtn.setAttribute("data-bs-toggle", "modal");
             deleteBtn.setAttribute("data-bs-target", "#deleteConfirm");
             deleteBtn.appendChild(deleteIcon);
             deleteBtn.setAttribute('data-row-index', i);
+            deleteBtn.setAttribute('disabled', true);
+            checkCreatted.addEventListener('change', function () {
+                // Check if the checkbox is checked
+                if (checkCreatted.checked) {
+                    // Enable the delete button
+                    // deleteBtn.removeAttribute('disabled');
+                    deleteBtn.toggleAttribute("disabled")
+                } else {
+                    deleteBtn.toggleAttribute("disabled")
+                    // deleteBtn.setAttribute('disabled', true);
+                }
+            });
             deleteBtn.addEventListener('click', function () {
-                let index = deleteBtn.getAttribute('data-row-index');
-                users.splice(index, 1);
-                document.getElementById("deleteRow").addEventListener("click", function () {
-                    localStorage.setItem("users", JSON.stringify(users));
-                    deleteTable();
-                    DrawTable(users, rowsNum.value);
-
-                });
-
+                document.getElementById("deleteRow").addEventListener("click", deleteRow)
             }); //delete button
 
             lastcreatedtd.appendChild(deleteBtn);
@@ -179,25 +124,37 @@ window.addEventListener('load', function () {
             userTable_tbody.appendChild(createdrow);
         }
     } //end of draw table
-
-    let search = document.querySelector("input[name=searchByName]");
-    search.addEventListener("input", searching);
-
-    function searching(e) {
-        //  console.log(e);
+    let searchInput = document.getElementById("searchTerm");
+    let searchTypeSelect = document.getElementById("searchType");
+    searchInput.addEventListener("input", searching);
+    function searching() {
+        let searchTerm = searchInput.value.toLowerCase();
+        let searchType = searchTypeSelect.value;
         let tr = document.querySelectorAll("tbody tr");
+
         for (let i = 0; i < tr.length; i++) {
             let node = tr[i];
-            let textContent = node.children[2].textContent.toLowerCase();
-            let show = textContent.indexOf(this.value.toLowerCase()) !== -1;
+            let textContent = "";
+
+            if (searchType === "id") {
+                textContent = node.children[1].textContent.toLowerCase(); 
+            } else if (searchType === "name") {
+                textContent = node.children[2].textContent.toLowerCase(); 
+            }
+
+            let show = textContent.indexOf(searchTerm) !== -1;
             node.style.display = show ? "" : "none";
         }
-    } //end of search
-
+    }
     let deleteAllBtn = document.getElementById("deleteAllBtn");
+    deleteAllBtn.addEventListener("click",function(){
+        document.getElementById("deleteRow").addEventListener("click", deleteRow)
+
+    });
     var checked = [];
-    document.getElementById("deleteRow").addEventListener("click", function () {
+    function deleteRow() {
         let indeces = [];
+        // console.log(checked);
         for (let i = 0; i < checked.length; i++) {
             indeces.push(checked[i].parentNode.parentNode.getAttribute("data-row-index"));
         }
@@ -209,7 +166,7 @@ window.addEventListener('load', function () {
         DrawTable(users, rowsNum.value);
         checked = [];
         deleteAllBtn.disabled = true;
-    });
+    }
     let checkAll = document.getElementById("checkAll");
     checkAll.addEventListener('click', function (e) {
         if (checkAll.checked) {
@@ -230,31 +187,6 @@ window.addEventListener('load', function () {
             return trs.parentNode.removeChild(trs);
         })
     }
-
-
-    //let addNewUser = document.getElementById("addUser");
-    // addNewUser.addEventListener("click", display);
-
-    // function display() {
-    //     let userFirstName = document.getElementById("user-fname");
-    //     let userLastName = document.getElementById("user-lname");
-    //     let userEmail = document.getElementById("user-email");
-    //     let userPass = document.getElementById("user-pass");
-    //     let userAge = document.getElementById("user-age");
-    //     let userRole = document.getElementById("user-role");
-    //     let userImagePath = document.getElementById("user-image");
-
-    //     let item = new userClass(userFirstName.value, userLastName.value, userEmail.value, userPass.value, userAge.value, userImagePath.value, userRole.value=="user"?0:1).addjson();
-    //     item.id = userClass.autoincreaseid();
-
-    //     // usersfullData.push(item);
-
-    //     localStorage.setItem("users", JSON.stringify(usersfullData));
-    //     usersfullData = JSON.parse(localStorage.getItem("users"));
-    //     deleteTable();
-    //     DrawTable(users, rowsNum.value);
-    // }
-
     document.getElementById('tbodyUsers').addEventListener('click', function (event) {
         let target = event.target;
         if (target.tagName === 'SPAN' && target.classList.contains('text-bg-success')) {
@@ -271,7 +203,47 @@ window.addEventListener('load', function () {
             checked = Array.from(document.querySelectorAll("input[name=checkRow]:checked"));
             deleteAllBtn.disabled = checked.length <= 1;
         }
+        if(target.closest ("button")){
+            row_id = target.parentNode.getAttribute('data-row-index');
+        }
     });
+
+    document.getElementById("editUser").addEventListener('click', function (e) {
+        let allinp = document.querySelectorAll('#editUserModal input');
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        console.log(allinp[0].value);
+        if (!isvalidvisaname(allinp[0].value)) {
+            showToast2("enter a valid First name", 3000, "#ea6060");
+        } else if (!isvalidvisaname(allinp[1].value)) {
+           // console.log(allinp[1].value);
+            showToast2("enter a valid Last name", 3000, "#ea6060");
+        } else if (!validateEmail(allinp[2], 'Email is required and must be a valid email address!')) {
+            e.preventDefault();
+        } else if (!passwordRegex.test(allinp[3].value)) {
+            showToast2("enter a valid Password, and must contain(a-z | A-Z | @$!%*?&) and greater than or equal 8 letters or numbers", 3000, "#ea6060");
+        } else if (allinp[4].value < 19) {
+            showToast2("Enter a valid age, and must be greater than or equal 19 years old", 3000, "#ea6060");
+        }else {
+            
+            let imgarr =allinp[5]? [getfilelocation(allinp[5])]:users[row_id].images;
+            console.log( users[row_id]);
+            users[row_id].fname = allinp[0].value;
+            console.log( users[row_id]);
+            users[row_id].lname = allinp[1].value;
+            users[row_id].email = allinp[2].value;
+            users[row_id].password = allinp[3].value;
+            users[row_id].age = allinp[4].value;
+            users[row_id].images = imgarr;
+            users[row_id].role = document.getElementById("user-role").value == "user" ? 0 : 1;
+
+            localStorage.setItem("users", JSON.stringify(users));
+            users = JSON.parse(localStorage.getItem("users"));
+            console.log(users);
+            deleteTable();
+            DrawTable(users, rowsNum.value);
+            e.target.parentElement.children[0].click();
+        }
+    })
 
     function displayProductsModal(products) {
         var tableBody = document.getElementById('tableBody');
@@ -306,84 +278,27 @@ window.addEventListener('load', function () {
         } else if (!validateEmail(allinp[2], 'Email is required and must be a valid email address!')) {
             e.preventDefault();
         } else if (!passwordRegex.test(allinp[3].value)) {
-            showToast("enter a valid Password, and must contain(a-z | A-Z | @$!%*?&)", 3000, "#ea6060");
+            showToast("enter a valid Password, and must contain(a-z | A-Z | @$!%*?&) and greater than or equal 8 letters or numbers", 3000, "#ea6060");
         } else if (allinp[4].value < 19) {
             showToast("Enter a valid age, and must be greater than or equal 19 years old", 3000, "#ea6060");
         } else if (allinp[5].value == "") {
             showToast("Enter a valid image", 3000, "#ea6060");
         } else {
-
             let imgarr = [getfilelocation(allinp[5])];
-            let role=document.getElementById("user-role").value == "user" ? 0 : 1 ;
+            let role = document.getElementById("user-role").value == "user" ? 0 : 1;
             console.log(imgarr);
-            allinp = Array.from(allinp);
-            let o = new userClass(allinp[0].value, allinp[1].value, allinp[2].value, allinp[3].value, allinp[4].value, imgarr, role);
-            //o.id = userClass.autoincreaseid();
-          console.log(o);
-
-            // usersfullData.push(item);
+            let o = new userClass(allinp[0].value, allinp[1].value, allinp[2].value, allinp[3].value, allinp[4].value, imgarr, role).addjson();
             users.push(o);
             localStorage.setItem("users", JSON.stringify(users));
             users = JSON.parse(localStorage.getItem("users"));
             console.log(users);
             deleteTable();
             DrawTable(users, rowsNum.value);
-            // e.target.parentElement.children[0].click();
+            e.target.parentElement.children[0].click();
         }
 
 
     })
-
-    // document.getElementById("editUser").addEventListener('click', function (e) {
-    //     let allinp = document.querySelectorAll('#editproducts input');
-    //     if (!isvalidvisaname(allinp[0].value)) {
-    //         showToast2("enter a valid Title", 3000, "#ea6060");
-    //     } else if (allinp[1].value <= 0) {
-    //         showToast2("enter a number in stock", 3000, "#ea6060");
-    //     } else if (allinp[2].value <= 0) {
-    //         showToast2("enter a valid price", 3000, "#ea6060");
-    //     } else if (document.getElementById('textarea2').value.trim() == "") {
-    //         showToast2("enter a description", 3000, "#ea6060");
-    //     } else if (!isvalidvisaname(allinp[3].value)) {
-    //         showToast2("enter a valid shape name", 3000, "#ea6060");
-    //     } else if (!isvalidvisaname(allinp[4].value)) {
-    //         showToast2("enter a frame color", 3000, "#ea6060");
-    //     } else if (!isvalidvisaname(allinp[5].value)) {
-    //         showToast2("enter a valid Lense glasses", 3000, "#ea6060");
-    //     } else if (!isvalidvisaname(allinp[6].value)) {
-    //         showToast2("enter a enter a valid treatment", 3000, "#ea6060");
-    //     } else {
-    //         //all_products = getlocal("products");
-    //         let ind = searchbyid(all_products, row_id);
-
-    //         if (allinp[7].value) {
-    //             products[ind].images[0] = getfilelocation(allinp[7]);
-    //         }
-    //         if (allinp[8].value) {
-    //             products[ind].images[1] = getfilelocation(allinp[8]);
-    //         }
-
-    //         products[ind].productTitle = allinp[0].value;
-    //         products[ind].stockQuantity = allinp[1].value;
-    //         products[ind].price = allinp[2].value;
-    //         products[ind].productDescription = document.getElementById('textarea2').value.trim();
-    //         products[ind].price = allinp[2].value;
-    //         products[ind].shape = allinp[3].value;
-    //         products[ind].frameColor = allinp[4].value;
-    //         products[ind].lensClass = allinp[5].value;
-    //         products[ind].treatment = allinp[6].value;
-
-    //         setlocal(all_products, "products");
-    //         current_products = all_products.filter((ele) => {
-    //             return ele.sellerId == current_user_id
-    //         });
-
-    //         createtablebody(current_products);
-    //         document.getElementById('searchByName').value = "";
-    //         e.target.parentElement.children[0].click();
-
-    //     }
-    // })
 
     function isvalidvisaname(input) {
         const usernameRegex = /^[a-zA-Z0-9-' ]{3,30}$/;
@@ -436,7 +351,7 @@ window.addEventListener('load', function () {
         el.style.display = 'block';
         el.innerText = message;
         el.style.backgroundColor = color;
-        document.getElementById('edituser').appendChild(el);
+        document.getElementById('editUser').appendChild(el);
         setTimeout(function () {
             document.querySelector('.toast').remove();
         }, duration);
